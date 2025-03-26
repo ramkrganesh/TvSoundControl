b10983637727a16cb14d2d79e036dbd20846a705
rravich <ramkrganesh@gmail.com>
2025-03-26 20:43:47 +0100

    created a header file; code to read Sound using ADC A0

--- tv_sound_ctrl/tv_sound_ctrl.ino
+++ tv_sound_ctrl/tv_sound_ctrl.ino
@@ -1,36 +1,11 @@
 /* Controlling TV using IR NEC protocol */
 
-//- MACRO Definitions ---------------------------------------------------------
-#define IR_LED            5u       // Pin 5 has PWM capabilities.
-#define CARRIER_FREQ      38000u  // 38khz carrier freq. NEC Protocol.
-#define LEAD_BURST_DUR_MS 9u      // 9ms Lead pulse duration.
-#define LEAD_LOW_PULSE_US 4500u   // 4500uS or 4.5mS duration for LOW time.
-#define LOG_1_0_TX_US     (563u - 25u)    // 563uS burst time to transmit '1' (562.2uS)
-#define LOG_1_TX_LOW_US   (1687u - 47u)   // 1.687ms delay
-#define LOG_0_TX_LOW_US   LOG_1_0_TX_US // 563uS low period
-// #define INIT_BURST_DELAY  (LEAD_BURST_DUR_MS - 3u)
-
-//- Following Macros are for testing
-#define PIN_INCREASE_VOL  7u
-#define PIN_DECREASE_VOL  4u
-
-//- Type Definitions  ---------------------------------------------------------
-typedef long  uint32;
-typedef long  Message_Type;
-typedef int   uint16;
-typedef byte  uint8;
-
-typedef enum {
-  INCREASE_VOLUME,
-  DECREASE_VOLUME,
-  DO_NOTHING,
-}Volume_Ctrl_Type;
+//- Header Includes -----------------------------------------------------------
+#include "tv_sound_ctrl.h"
 
 //- Global Variables  ---------------------------------------------------------
 uint32 IncreaseVolume_u32 = 0xE51ABF40; // Send the msg, LSB first
 uint32 DecreaseVolume_u32 = 0xE11EBF40;
-
-//- Inline Functions  ---------------------------------------------------------
 
 //- Global Functions  ---------------------------------------------------------
 
@@ -44,7 +19,7 @@ uint32 DecreaseVolume_u32 = 0xE11EBF40;
 * Parameters: None
 * Returns: None
 *--------------------------------------------------------------------------- */
-void StartComm(void)
+static inline void StartComm(void)
 {
   tone(IR_LED, CARRIER_FREQ);
   delay(LEAD_BURST_DUR_MS);
@@ -84,11 +59,10 @@ static inline void StopComm(void)
 void TransmitData(Message_Type* Cmd)
 {
   StartComm();
-  
   for(uint8 bit_pos = 0u; bit_pos < 32; bit_pos++)
   {
       tone(IR_LED, CARRIER_FREQ);
-      delayMicroseconds(270u); // edit here -----------------------------------
+      delayMicroseconds(270u); // edit here 562.2uS---------------------------
       noTone(IR_LED);
       digitalWrite(IR_LED, LOW);    
     
@@ -103,8 +77,24 @@ void TransmitData(Message_Type* Cmd)
       delayMicroseconds(LOG_0_TX_LOW_US);
     }
   }
-
   StopComm();
+
+  for(uint8 idx = 0; idx < 4u; idx++)  /*Prepare Repeat signal: 4 times*/
+  {
+    delay(REPEAT_SIG_DELAY_MS);
+    
+    tone(IR_LED, CARRIER_FREQ);
+    delay(LEAD_BURST_DUR_MS);
+    noTone(IR_LED);
+    
+    digitalWrite(IR_LED, LOW);
+    delayMicroseconds(REPEAT_SIG_SPACE_US);
+
+    tone(IR_LED, CARRIER_FREQ);
+    delayMicroseconds(270u); // edit here 560uS-----------------------------
+    noTone(IR_LED);
+    digitalWrite(IR_LED, HIGH);
+  }
 }
 
 /* ----------------------------------------------------------------------------
@@ -151,29 +141,33 @@ void ControlTv(Volume_Ctrl_Type Action)
 //- Init Function -------------------------------------------------------------
 void setup() {
   pinMode(IR_LED, OUTPUT);
-  pinMode(PIN_INCREASE_VOL, INPUT);
-  pinMode(PIN_DECREASE_VOL, INPUT);
   digitalWrite(IR_LED, HIGH);
-  Serial.begin(9600);
+  
+  #if CFG_TESTS_EN
+    pinMode(PIN_INCREASE_VOL, INPUT);
+    pinMode(PIN_DECREASE_VOL, INPUT);
+    Serial.begin(9600);
+  #endif
 }
 
 void loop() {
-  Volume_Ctrl_Type Action = DO_NOTHING;
+  uint16 Tv_Volume_raw    = 0u;
 
+  Tv_Volume_raw = (uint16)analogRead(SOUND_IN_ADC);
+  
+#if CFG_TESTS_EN
   if(digitalRead(PIN_INCREASE_VOL) == HIGH)
   {
     Serial.println("Button Pressed");
     delay(3000);
-    Action = INCREASE_VOLUME;
+#else
+  if(Tv_Volume_raw >= VOLUME_THRESHOLD)
+  {
+#endif
+
+    ControlTv(DECREASE_VOLUME);
+    delay(VOL_DECR_WAIT_MS);
+    ControlTv(INCREASE_VOLUME);
   }
 
-  if(digitalRead(PIN_DECREASE_VOL) == HIGH)
-  {
-    delay(3000);
-    Action = DECREASE_VOLUME;
-  }
-  
-  // put your main code here, to run repeatedly:
-  ControlTv(Action);
-  Action = DO_NOTHING; //- test code
 }
